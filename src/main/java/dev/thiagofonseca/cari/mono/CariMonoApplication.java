37c256c6c783d37dce20169f1010c9a5554d2c2b
Thiago Fonseca <dev.thiago@gmail.com>
2023-01-29 20:37:03 -0300

    Initial structure

---
+++ src/main/java/dev/thiagofonseca/cari/mono/CariMonoApplication.java
@@ -0,0 +1,13 @@
+package dev.thiagofonseca.cari.mono;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CariMonoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CariMonoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/thiagofonseca/cari/mono/CariMonoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.thiagofonseca.cari.mono;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CariMonoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
