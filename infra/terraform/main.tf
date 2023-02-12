366c691b1af4de8422b7a08895ace0bf988a8129
Thiago Fonseca <dev.thiago@gmail.com>
2023-02-12 13:19:10 -0300

    Creating alarm notification in terraform

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -1,3 +1,5 @@
+######### Database
+
 resource "aws_security_group" "allow_access_cori_db" {
   name = "allow_access_cori_db"
   ingress {
@@ -21,3 +23,75 @@ resource "aws_db_instance" "cori_db_instance" {
   publicly_accessible    = true
   skip_final_snapshot    = true
 }
+
+
+######### Queues
+resource "aws_sqs_queue" "sqs_test" {
+  name                      = "sqs_test"
+  receive_wait_time_seconds = 10
+}
+
+resource "aws_sqs_queue" "sqs_test_dlq" {
+  name = "sqs_test_dlq"
+}
+
+resource "aws_sqs_queue_redrive_policy" "sqs_test_redrive_policy" {
+  queue_url = aws_sqs_queue.sqs_test.id
+
+  redrive_policy = jsonencode({
+    deadLetterTargetArn = aws_sqs_queue.sqs_test_dlq.arn
+    maxReceiveCount     = 4
+  })
+
+  depends_on = [
+    aws_sqs_queue.sqs_test,
+    aws_sqs_queue.sqs_test_dlq
+  ]
+}
+
+resource "aws_sqs_queue_redrive_allow_policy" "sqs_test_dlq_redrive_allow_policy" {
+  queue_url = aws_sqs_queue.sqs_test_dlq.id
+
+  redrive_allow_policy = jsonencode({
+    redrivePermission = "byQueue",
+    sourceQueueArns   = [aws_sqs_queue.sqs_test.arn]
+  })
+
+  depends_on = [
+    aws_sqs_queue.sqs_test,
+    aws_sqs_queue.sqs_test_dlq
+  ]
+}
+
+
+######### Notifications
+
+resource "aws_sns_topic" "sns_test" {
+  name = "sns_test"
+}
+
+resource "aws_sns_topic_subscription" "sns_test_subscription" {
+  topic_arn = aws_sns_topic.sns_test.arn
+  protocol  = "email"
+  endpoint  = var.CORI_ALARM_USER
+}
+
+
+######### Alarm
+
+resource "aws_cloudwatch_metric_alarm" "dlq_new_message_alarm" {
+  alarm_name          = "dlq_new_message_alarm"
+  statistic           = "Sum"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  threshold           = 1
+  period              = 60
+  evaluation_periods  = 2
+  namespace           = "AWS/SQS"
+  metric_name         = "ApproximateNumberOfMessagesVisible"
+  dimensions = {
+    QueueName = aws_sqs_queue.sqs_test_dlq.name
+  }
+  alarm_actions = [aws_sns_topic.sns_test.arn]
+  ok_actions    = [aws_sns_topic.sns_test.arn]
+}
+
--- infra/terraform/vars.tf
+++ infra/terraform/vars.tf
@@ -6,6 +6,10 @@ variable "CORI_DATABASE_PASSWORD" {
   type = string
 }
 
+variable "CORI_ALARM_USER" {
+  type = string
+}
+
 variable "ALLOWED_IPS" {
   type = list(string)
 }
